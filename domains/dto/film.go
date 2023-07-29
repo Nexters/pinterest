2872cd79c271be444e4f73e61f709fcdf23e6afc
chaewonkong <chaewonkong@gmail.com>
2023-07-29 17:50:17 +0900

    fix: 🐛 remove unused

--- domains/dto/film.go
+++ domains/dto/film.go
@@ -1,13 +1,9 @@
 package dto
 
 type Film struct {
-	Type          string `json:"type"`
 	Title         string `json:"title" validate:"required"`
-	Text          string `json:"text"`
-	Image         string `json:"image"`
 	Order         uint   `json:"order"`
 	PhotoCutCount uint   `json:"photo_cut_count" validate:"gte=0"`
 	Likes         uint   `json:"likes" validate:"gte=0"`
-	Link          string `json:"link"`
 	UserID        string `json:"user_id"`
 }
