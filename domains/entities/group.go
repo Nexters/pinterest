7e5e9d8ce1cec0c6843aa57951847141ecb1d15e
맹순영 <symaeng98@naver.com>
2023-07-29 16:33:56 +0900

    fix: change group entity

--- domains/entities/group.go
+++ domains/entities/group.go
@@ -4,14 +4,10 @@ import "gorm.io/gorm"
 
 type Group struct {
 	gorm.Model
-	Type      string `gorm:"not null"`
 	Title     string `gorm:"not null"`
-	Text      string
-	Image     string `gorm:"not null"`
 	Order     uint   `gorm:"not null"`
 	ItemCount uint
 	Likes     uint
-	Link      string
 	UserID    uint
 	Items     []Item
 }
