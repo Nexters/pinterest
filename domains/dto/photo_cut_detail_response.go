ae5102b8e5f66df63f84c96a6b26ce671ccdb360
맹순영 <symaeng98@naver.com>
2023-08-01 00:10:33 +0900

    fix: remove validator

--- domains/dto/photo_cut_detail_response.go
+++ domains/dto/photo_cut_detail_response.go
@@ -3,11 +3,11 @@ package dto
 import "time"
 
 type PhotoCutDetailResponse struct {
-	Title     string    `json:"title" validate:"required"`
+	Title     string    `json:"title"`
 	Text      string    `json:"text"`
 	Link      string    `json:"link"`
 	Image     string    `json:"image"`
-	Likes     uint      `json:"likes" validate:"gte=0"`
+	Likes     uint      `json:"likes"`
 	FilmID    uint      `json:"film_id"`
 	CreatedAt time.Time `json:"created_at"`
 }
