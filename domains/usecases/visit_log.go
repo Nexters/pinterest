044a9f752d7599767cfa6df22143ad79756b0c0b
chaewonkong <chaewonkong@gmail.com>
2023-08-06 18:06:07 +0900

    refactor: 💡 implement dto for visit log

--- domains/usecases/visit_log.go
+++ domains/usecases/visit_log.go
@@ -15,13 +15,24 @@ func NewVisitLogService(repo *entities.VisitLogRepository) *VisitLogService {
 	return &VisitLogService{repo}
 }
 
-func (v *VisitLogService) FindAll(ctx context.Context, UserID string) ([]entities.VisitLog, error) {
-	return v.repo.FindAllVisitLogsByUserID(ctx, UserID)
+func (v *VisitLogService) FindAll(ctx context.Context, UserID string) (visitLogs []dto.VisitLogResponse, err error) {
+	logs, err := v.repo.FindAllVisitLogsByUserID(ctx, UserID)
+	if err != nil {
+		return
+	}
+
+	for _, log := range logs {
+		v := dto.VisitLogResponse{}
+		visitLogs = append(visitLogs, v.FromEntity(log))
+	}
+
+	return
 }
 
-func (v *VisitLogService) Create(ctx context.Context, dto dto.VisitLogCreationRequest) (log entities.VisitLog, err error) {
+func (v *VisitLogService) Create(ctx context.Context, dto dto.VisitLogCreationRequest) (res dto.VisitLogCreationResponse, err error) {
 	logParam := dto.ToEntity()
-	log, err = v.repo.CreateVisitLog(ctx, logParam)
+	log, err := v.repo.CreateVisitLog(ctx, logParam)
+	res = res.FromEntity(log)
 	return
 }
 
