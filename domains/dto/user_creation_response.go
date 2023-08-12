d56399e005dd40d46ed6eea1498f3ade0a16e456
chaewonkong <chaewonkong@gmail.com>
2023-08-13 01:39:52 +0900

    fix: 🐛 add json tag

--- domains/dto/user_creation_response.go
+++ domains/dto/user_creation_response.go
@@ -3,11 +3,10 @@ package dto
 import "time"
 
 type UserCreationResponse struct {
-	Name       string
-	UserID     string
-	Email      string
-	Visitors   uint
-	ThemeColor string
-	Text       string
-	CreatedAt  time.Time
+	Name       string    `json:"name"`
+	UserID     string    `json:"user_id"`
+	Visitors   uint      `json:"visitors"`
+	ThemeColor string    `json:"theme_colors"`
+	Text       string    `json:"text"`
+	CreatedAt  time.Time `json:"created_at"`
 }
